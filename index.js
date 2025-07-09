8d25efe9649fa83baba30ba06782bd0a9ed2dcc6
lesterlw <lesterlw@hotmail.com>
2025-07-09 13:26:33 -0400

    temporary inline code

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json());   // creates http server
 const token = 'M92d-M4FC55PufZ8erQOdLUUI!hx?rBU2qprZH=TmDafn7Mz';
 
-// custom modules:
-const subtractHours = require('./utility');
-const isWeekend = require('./utility');
-const isAfterHours = require('./utility');
-//
-
 // webhook check:
 app.get('/', (req, res) => {
     // check if verification token is correct
@@ -25,30 +19,56 @@ app.get('/', (req, res) => {
 
 // webhook endpoint:
 app.post('/', (req, res) => {
+    
+    // custom modules:
+    //const isWeekend = require('./utility');
+    //const isAfterHours = require('./utility');
+    //
+
     // check if verification token is correct
     if (req.query.token !== token) {
         return res.sendStatus(401); // if not, return unauthorized error
+    }
+
+    // check for favicon
+    if (req.url?.endsWith("favicon.ico")) {
+        return res.sendStatus(404);
     }
 
     // initialize office hours status value
     var isOfficeHours = "1";
     //
 
-    // get current date/time:
-    var dteNowAST = subtractHours(new Date(), 4);     // convert date to AST
-    console.log(Date(dteNowAST));
-    //
-
     // check for weekend:
-    if (isWeekend(dteNowAST)) {
+    // if (isWeekend()) {
+    //    isOfficeHours = "0";
+    // }
+
+    // isWeekend if Saturday (6) or Sunday (0)
+    var dateDOW = new Date();
+    var intDayOfWeek = dateDOW.getDay();
+    console.log('Day:');
+    console.log(intDayOfWeek);
+    if ((intDayOfWeek === 6) || (intDayOfWeek === 0)) {
         isOfficeHours = "0";
     }
+
     //
 
     // check for after-hours:
-    if (isAfterHours(dteNowAST)) {
+    // if (isAfterHours()) {
+    //    isOfficeHours = "0";
+    // }
+
+    // isAfterHours if Hour < 8 or Hour >= 16
+    var dateAH = new Date();
+    var intHour = dateAH.getHours();
+    console.log('Hour:');
+    console.log(intHour);
+    if ((intHour < 8) || (intHour >= 16)) {
         isOfficeHours = "0";
     }
+    
     //
 
     // check for holiday:
--- utility.js
+++ utility.js
@@ -1,28 +1,32 @@
 function subtractHours(date, hours) {
-    date.setHours(new Date(date).getHours() - hours);
+    date.setHours(date.getHours() - hours);
     return date;
 }
 
 
-function isWeekend(date) {
+function isWeekend() {
     // isWeekend if Saturday (6) or Sunday (0)
-    var intDayOfWeek = new Date(date).getDay();
+    var date = new Date();
+    var intDayOfWeek = date.getDay();
+    console.log('Day:');
+    console.log(intDayOfWeek);
     return (intDayOfWeek === 6) || (intDayOfWeek === 0);
 }
 
 
-function isAfterHours(date) {
+function isAfterHours() {
     // isAfterHours if Hour < 8 or Hour >= 16
-    var intHour = new Date(date).getHours();
-    return (intHour < 8) || (intHour >= 16);
+    var date = new Date();
+    var intHour = date.getHours();
+    console.log('Hour:');
+    console.log(intHour);
+    return ((intHour < 8) || (intHour >= 16));
 }
 
 
 
 
 
-
-module.exports = subtractHours;
-module.exports = isWeekend;
-module.exports = isAfterHours;
+// module.exports = isWeekend;
+// module.exports = isAfterHours;
 
